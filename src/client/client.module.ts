7f098cf281c4f89bd2319ce344fb25dd3a30c197
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-25 18:13:52 +0200

    Create client service

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientController } from './controller/client.controller';
+import { ClientService } from './service/client.service';
 
 @Module({
   controllers: [ClientController],
+  providers: [ClientService],
 })
 export class ClientModule {}
