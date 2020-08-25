4e299ea2f8603dc983c742fa29e05c0fdabb12b1
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-25 18:42:04 +0200

    Create module client assignation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TrainerModule } from './trainer/trainer.module';
 import { ClientModule } from './client/client.module';
+import { ClientAssignationModule } from './client-assignation/client-assignation.module';
 
 @Module({
-  imports: [TrainerModule, ClientModule],
+  imports: [TrainerModule, ClientModule, ClientAssignationModule],
 })
 export class AppModule {}
