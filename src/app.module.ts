77ad3fcbf92e3d58961f0c14f8bb6bc58a039fbf
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-25 18:11:57 +0200

    Remove unsued code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TrainerModule } from './trainer/trainer.module';
 
 @Module({
   imports: [TrainerModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
