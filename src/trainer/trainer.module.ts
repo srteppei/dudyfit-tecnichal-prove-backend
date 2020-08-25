01b77ec8109e8220ce9086896129359cb2171f30
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-25 17:10:19 +0200

    Generate service trainer

--- src/trainer/trainer.module.ts
+++ src/trainer/trainer.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TrainerController } from './controller/trainer/trainer.controller';
+import { TrainerService } from './service/trainer.service';
 
 @Module({
-  controllers: [TrainerController]
+  controllers: [TrainerController],
+  providers: [TrainerService]
 })
 export class TrainerModule {}
