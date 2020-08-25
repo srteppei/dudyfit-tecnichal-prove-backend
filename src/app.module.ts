22c621c5794351064c930355985ba4099c9f1f3f
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-25 18:12:41 +0200

    Create module client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TrainerModule } from './trainer/trainer.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [TrainerModule],
+  imports: [TrainerModule, ClientModule],
 })
 export class AppModule {}
