ef25380b008df66c5521be1172114ef6fbee8081
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-08-25 17:06:08 +0200

    Create module trainer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrainerModule } from './trainer/trainer.module';
 
 @Module({
-  imports: [],
+  imports: [TrainerModule],
   controllers: [AppController],
   providers: [AppService],
 })
